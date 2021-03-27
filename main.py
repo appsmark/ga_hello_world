3d6224ca7e68ce08ea1b54083695321968736eb0
appsmark <75047596+appsmark@users.noreply.github.com>
2021-03-27 21:46:52 +0100

    Random characters

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+import random
+
 def create():
     round = 0
     round_maximum = 10
     while(round < round_maximum):
         round += 1
         print('Hello World')
+        output = ""
+        for index in range(11):
+            output += chr(65 + random.randrange(26) + 32 * random.randrange(2))
+        print(output)
 
 if __name__ == '__main__':
     print('GA Hello World')
